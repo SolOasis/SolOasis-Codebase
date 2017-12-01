54fd774b04ca4f002865417a9f2d164269b14395
cdgood <cdgood29@gmail.com>
2017-12-01 13:23:40 -0500

    Minor changes

--- SolOasis/ControlModule.h
+++ SolOasis/ControlModule.h
@@ -2,9 +2,18 @@
 #define _CONTROL_MODULE_H_
 
 #include "ControlIntfc.h"
+#include "Globals.h"
+
+#ifdef DEBUG
+#include "Debug.h"
+#endif
 
 class ControlModule : public ControlIntfc {
   private:
+#ifdef DEBUG
+	Debug debug;
+#endif
+
     Status rotateHorizontal(int dgr);
     Status rotateVertical(int dgr);
 
